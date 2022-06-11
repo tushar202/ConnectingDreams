370f9ebe7396671c300aaca674581f9b83f38040
Anushka Aggarwal <aanushka357@gmail.com>
2022-06-11 21:17:52 +0530

    feat: home page ui

--- Code/client/src/components/homePage/Content.js
+++ Code/client/src/components/homePage/Content.js
@@ -1,9 +1,113 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Layout, Tabs } from "antd";
 
-const Content = () => {
-  return (
-    <div>HomePage</div>
-  )
+const { Content, Footer } = Layout;
+const { TabPane } = Tabs;
+
+function debounce(fn, ms) {
+  let timer;
+  return (_) => {
+    clearTimeout(timer);
+    timer = setTimeout((_) => {
+      timer = null;
+      fn.apply(this, arguments);
+    }, ms);
+  };
 }
 
-export default Content;
+const ContentPage = () => {
+  const history = useNavigate();
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+  const [activeTabKey, setActiveTabkey] = useState("1");
+  const [category, setCategory] = useState("");
+
+  useEffect(() => {
+    const debouncedHandleResize = debounce(function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    }, 1000);
+    window.addEventListener("resize", debouncedHandleResize);
+    return (_) => {
+      window.removeEventListener("resize", debouncedHandleResize);
+    };
+  });
+
+
+  const onCategorySelect = (categoryName) => {
+    setCategory(categoryName);
+  };
+
+  const onCategoryDeselect = () => {
+    setCategory("");
+  };
+
+  return (
+    <>
+      <style type="text/css">
+        {`
+          .ant-layout{
+            min-height: 90vh;
+          }
+          .ant-tabs-ink-bar {
+            background-color: #B4C71E;
+          }
+          .ant-tabs-ink-bar-animated {
+            background-color: #B4C71E;
+          }
+          .ant-tabs-tab.ant-tabs-tab-active .ant-tabs-tab-btn, .ant-tabs-tab:hover  {
+            color: #B4C71E;
+          }
+          .ant-breadcrumb-link:hover {
+            cursor: pointer;
+            color: black;
+          }
+        `}
+      </style>
+
+      <Layout className="layout">
+        <Content
+          style={
+            dimensions.width < 500
+              ? { padding: "0 10px" }
+              : { padding: "0 30px" }
+          }
+        >
+          <div className="site-layout-content">
+            {(activeTabKey === "1" && category === "") && <div style={{display: 'flex', justifyContent: 'center'}}>
+              {/* <SearchBar /> */}
+            </div>}
+            <Tabs
+              onChange={(activeKey) => {
+                setActiveTabkey(activeKey);
+              }}
+              tabPosition={dimensions.width < 500 ? "top" : "left"}
+            >
+              <TabPane tab="Categories" key="1">
+                {category === "" ? (
+                  <div categorySelectHandler={onCategorySelect} />
+                ) : (
+                  <div categoryName={category} />
+                )}
+              </TabPane>
+              <TabPane tab="Favorites" key="2">
+                <div />
+              </TabPane>
+              <TabPane tab="My Forms" key="3">
+                <div />
+              </TabPane>
+            </Tabs>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}></Footer>
+      </Layout>
+    </>
+  );
+};
+
+export default ContentPage;
--- Code/client/src/components/homePage/index.js
+++ Code/client/src/components/homePage/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import NavbarPage from "../navbarPage"
-import Content from "./Content"; 
+import ContentPage from "./Content"; 
 import {Link} from "react-router-dom";
 
 const HomePage = () => {
@@ -8,7 +8,7 @@ const HomePage = () => {
     <>
       <NavbarPage />
       <br />
-      <Content />
+      <ContentPage />
       <Link to="/form1">form1</Link>
       <Link to="/form2">form2</Link>
       <Link to="/form3">form3</Link>
