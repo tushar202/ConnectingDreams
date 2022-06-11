32a43a403ee5a41fd4facc76224f4e4c35275cda
Lakshita Chauhan <lakshitachauhan23@gmail.com>
2022-06-11 17:01:57 +0530

    feat : form cm added

---
+++ Code/client/src/components/proposedFormPage/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+import Form from "./Form";
+
+const CMFormPage = () => {
+  return (
+    <Container>
+      <Form />
+    </Container>
+  );
+};
+
+export default CMFormPage;
