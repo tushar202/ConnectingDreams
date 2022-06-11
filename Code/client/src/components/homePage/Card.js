2b6d091337f2ede68fa92ea455f4e5e70bff4573
aditya491929 <aditya.malwade@somaiya.edu>
2022-06-11 22:26:49 +0530

    feat: Cards Added

--- Code/client/src/components/homePage/Card.js
+++ Code/client/src/components/homePage/Card.js
@@ -1,21 +1,177 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
+import React, { useState, useEffect } from "react";
+import { Card } from "react-bootstrap";
+import axios from "axios";
+import Grow from "@mui/material/Grow";
 
 const CardPage = () => {
-  return (
-<><Card style={{ width: '18rem' }}>
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Card.Link href="#">Card Link</Card.Link>
-    <Card.Link href="#">Another Link</Card.Link>
-  </Card.Body>
-</Card></>
-  )
-}
+  const [caseStudies, setCaseStudies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-export default CardPage
+  useEffect(async () => {
+    const response = await axios.get(
+      `${process.env.REACT_APP_API_ENDPOINT}cdf/view`
+    );
+    if (response.data.length !== 0) {
+      setCaseStudies(response.data);
+    }
+    setIsLoading(false);
+  }, []);
+
+  return (
+    <>
+      {!isLoading && (
+        <Grow
+          in={!isLoading}
+          style={{ transformOrigin: "50 50 50 50" }}
+          {...(!isLoading ? { timeout: 1000 } : {})}
+        >
+          <Row
+            className="g-4"
+            style={{ display: "flex", justifyContent: "center" }}
+          >
+            {caseStudies.map((caseStudy) => {
+              return (
+                <Card style={{ width: "18rem" }}>
+                  <Card.Body>
+                    <div
+                      style={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Card.Title>
+                        <Tooltip placement="top" title={`~ ${form.username}`}>
+                          {caseStudy.title}
+                        </Tooltip>
+                      </Card.Title>
+                      <Card.Subtitle
+                        className="mb-2 text-muted"
+                        style={{ fontSize: "0.8em" }}
+                      >
+                        <Pill
+                          display="inline-flex"
+                          margin={0}
+                          color="green"
+                          style={{ display: "flex", alignItems: "center" }}
+                        >
+                          <CalendarOutlined style={{ marginRight: "3px" }} />{" "}
+                          {`${caseStudy.application_end_date.slice(0, 10)}`}
+                        </Pill>
+                      </Card.Subtitle>
+                    </div>
+                    <div
+                      style={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Card.Subtitle
+                        className="mb-2 text-muted"
+                        style={{ fontSize: "0.8em" }}
+                      >
+                        <Pill
+                          display="inline-flex"
+                          margin={0}
+                          style={{
+                            display: "flex",
+                            alignItems: "center",
+                            width: "fit-content",
+                          }}
+                        >
+                          <IdcardOutlined style={{ marginRight: "3px" }} />{" "}
+                          {caseStudy._id}
+                        </Pill>
+                      </Card.Subtitle>
+                      <Card.Subtitle
+                        className="mb-2 text-muted"
+                        style={{ fontSize: "0.8em" }}
+                      >
+                        <Pill
+                          display="inline-flex"
+                          margin={0}
+                          style={{
+                            display: "flex",
+                            alignItems: "center",
+                            width: "fit-content",
+                          }}
+                          color="red"
+                        >
+                          {caseStudy.tags[0]}
+                        </Pill>
+                      </Card.Subtitle>
+                    </div>
+
+                    <div
+                      style={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Card.Subtitle
+                        className="mb-2 text-muted"
+                        style={{ fontSize: "0.8em" }}
+                      >
+                        <Pill
+                          display="inline-flex"
+                          margin={0}
+                          style={{
+                            display: "flex",
+                            alignItems: "center",
+                            width: "fit-content",
+                          }}
+                        >
+                          <IdcardOutlined style={{ marginRight: "3px" }} />{" "}
+                          {caseStudy.location}
+                        </Pill>
+                      </Card.Subtitle>
+                      <Card.Subtitle
+                        className="mb-2 text-muted"
+                        style={{ fontSize: "0.8em" }}
+                      >
+                        <Pill
+                          display="inline-flex"
+                          margin={0}
+                          color="green"
+                          style={{ display: "flex", alignItems: "center" }}
+                        >
+                          <CalendarOutlined style={{ marginRight: "3px" }} />{" "}
+                          {`${caseStudy.createdAt.slice(0, 10)}`}
+                        </Pill>
+                      </Card.Subtitle>
+                    </div>
+                    <Card.Text>{form.description}</Card.Text>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Button
+                        variant={"dark"}
+                        onClick={() => {
+                          history(`/caseStudy/${caseStudy._id}`);
+                        }}
+                        size="sm"
+                      >
+                        Fill Form
+                      </Button>
+                    </div>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </Row>
+        </Grow>
+      )}
+    </>
+  );
+};
+
+export default CardPage;
